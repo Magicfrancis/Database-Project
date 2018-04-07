e2920c47c5462037e2031b331ea9fe525f2bde49
cusengan <lordvinh1@live.com>
2018-04-07 16:39:38 -0500

    Web page displays customer, director, and staff information

--- displayData.php
+++ displayData.php
@@ -14,14 +14,41 @@
             die("Connection failed: " . $conn->connect_error);
         }
 
-        $sql = "SELECT username FROM `customer`";
+        $sql = "SELECT Fname, Lname FROM person P JOIN customer C ON P.Ssn=C.Cust_Ssn;";
         $result = $conn->query($sql);
 
         echo 'Customers'. "<br>";
         if ($result->num_rows > 0) {
             // output data of each row
             while($row = $result->fetch_assoc()) {
-                echo "Name: " . $row["username"] . "<br>";
+                echo "Name: " . $row["Fname"] . " " . $row["Lname"] . "<br>";
+            }
+        } else {
+            echo "0 results";
+        }
+
+        $sql = "SELECT Fname, Lname FROM person P JOIN director D ON P.Ssn=D.D_Ssn;";
+
+        $result = $conn->query($sql);
+        echo "<br>";
+        echo 'Directors'. "<br>";
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "Name: " . $row["Fname"] . " " . $row["Lname"] . "<br>";
+            }
+        } else {
+            echo "0 results";
+        }
+
+        $sql = "SELECT Fname, Lname FROM person P JOIN staff S ON P.Ssn=S.S_Ssn";
+        $result = $conn->query($sql);
+        echo "<br>";
+        echo 'Staff'. "<br>";
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "Name: " . $row["Fname"] . " " . $row["Lname"] . "<br>";
             }
         } else {
             echo "0 results";
