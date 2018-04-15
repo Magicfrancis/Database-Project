4a7a6af84f4087207a31793005e975f7d67282c1
Francis <lefrancis12@yahoo.com>
2018-04-15 12:55:41 -0500

    have forms for updating sales price for all item

---
+++ updatePriceForm.php
@@ -0,0 +1,11 @@
+<html>
+	<body>
+    <form action="updatePrice.php" method="post">
+      Sale % OFF: <input type="text" name="sale"><br>
+      <input type="submit">
+    </form>
+		<?php include('goToHome.php');?>
+
+
+	</body>
+</html>
